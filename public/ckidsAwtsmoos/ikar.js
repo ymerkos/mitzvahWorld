48dd33234a5b28fa43bbc2043a20de6e2b56c717
ymerkos <121984148+ymerkos@users.noreply.github.com>
2024-03-13 02:49:51 +0000

    B"H

--- public/ckidsAwtsmoos/ikar.js
+++ public/ckidsAwtsmoos/ikar.js
@@ -14,7 +14,9 @@
 import ManagerOfAllWorlds from "./worldManager.js";
 
 console.log("B\"H");
-
+onerror = (msg, url ,ln) {
+    console.log("There was an error: ",msg,url,ln)
+}
 
 
 ///alert("Loaded")
