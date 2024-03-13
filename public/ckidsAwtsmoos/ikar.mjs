c2ebd9f17d2c645a5d051d711e4bbdae2f12027e
ymerkos <121984148+ymerkos@users.noreply.github.com>
2024-03-13 03:06:28 +0000

    BH

---
+++ public/ckidsAwtsmoos/ikar.mjs
@@ -0,0 +1,48 @@
+/**
+ * B"H
+
+ */
+
+/**
+ * import data for world
+ */
+
+
+
+//import starting level
+
+import ManagerOfAllWorlds from "./worldManager.mjs";
+
+console.log("B\"H",
+"\n","Starting the Ikar JS!")
+
+try {
+
+
+    ///alert("Loaded")
+    var m = new ManagerOfAllWorlds('/oyvedEdom.js');
+    window.mana =  m;
+    console.log("Loaded!",m)
+} catch(e) {
+    console.log("Issue!", e)
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
